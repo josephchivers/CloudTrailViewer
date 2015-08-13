c4822fabf51c11c58d1da0714c681884d3032a8f
josephchivers <joseph.chivers@yahoo.co.uk>
2015-08-13 10:09:29 +0100

    Allow directories to be used as a data source
    The existing app only allows zip files to be picked using the file
    chooser. This change allows directories to be picked then all files in
    all children of the picked directory are loaded into the app.

--- src/main/java/com/haskins/cloudtrailviewer/application/LoadToolBar.java
+++ src/main/java/com/haskins/cloudtrailviewer/application/LoadToolBar.java
@@ -18,6 +18,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 package com.haskins.cloudtrailviewer.application;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JToolBar;
+
 import com.haskins.cloudtrailviewer.CloudTrailViewer;
 import com.haskins.cloudtrailviewer.core.DbManager;
 import com.haskins.cloudtrailviewer.dialog.S3FileChooser;
@@ -25,18 +40,6 @@ import com.haskins.cloudtrailviewer.dialog.SearchOptions;
 import com.haskins.cloudtrailviewer.model.filter.Filter;
 import com.haskins.cloudtrailviewer.utils.ResultSetRow;
 import com.haskins.cloudtrailviewer.utils.ToolBarUtils;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.io.File;
-import java.util.List;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JToolBar;
 
 /**
  * Provides the Load / Scan Event Tool Bar
@@ -45,153 +48,190 @@ import javax.swing.JToolBar;
  */
 public class LoadToolBar extends JToolBar {
 
-    private final JFileChooser fileChooser = new JFileChooser();
-    
-    private final CloudTrailViewerApplication application;
-    
-    /**
-     * Default Constructor
-     * @param application reference to the application.
-     */
-    public LoadToolBar(CloudTrailViewerApplication application) {
-        
-        this.application = application;
-        fileChooser.setMultiSelectionEnabled(true);
-        
-        buildToolBar();
-    }
-        
-    ////////////////////////////////////////////////////////////////////////////
-    ///// private methods
-    ////////////////////////////////////////////////////////////////////////////
-    private void buildToolBar(){
-        
-        this.setFloatable(false);
-        this.setLayout(new BorderLayout());
-        this.setBackground(Color.WHITE);
-        
-        JPanel buttonsPanel = new JPanel();
-        buttonsPanel.setBackground(Color.WHITE);
-        buttonsPanel.setLayout(new BoxLayout(buttonsPanel, BoxLayout.LINE_AXIS));
-        
-        addLocalOpenButton(buttonsPanel);
-        addCloudDownloadButton(buttonsPanel);
-        addLocalScanButton(buttonsPanel);
-        addCloudScanButton(buttonsPanel);
-        
-        this.add(buttonsPanel, BorderLayout.WEST);
-    }
-    
-    private void addLocalOpenButton(JPanel buttonsPanel) {
-        
-        JButton btnLocal = new JButton();
-        ToolBarUtils.addImageToButton(btnLocal, "Local-Open-48.png", "Load", "Load Local Files");
-        btnLocal.addMouseListener(new MouseAdapter() {
+	private final JFileChooser fileChooser = new JFileChooser();
 
-            @Override
-            public void mouseClicked(MouseEvent mouseEvent) {
-                
-                loadLocalFiles(null);
-            }
-        }); 
-        
-        buttonsPanel.add(btnLocal);
-    }
-    
-    private void addLocalScanButton(JPanel buttonsPanel) {
-        
-        JButton btnLocal = new JButton();
-        ToolBarUtils.addImageToButton(btnLocal, "Local-Scan-48.png", "Local Scan", "Scan Local Files");
-        btnLocal.addMouseListener(new MouseAdapter() {
+	private final CloudTrailViewerApplication application;
 
-            @Override
-            public void mouseClicked(MouseEvent mouseEvent) {
-                
-                if (SearchOptions.showDialog(CloudTrailViewer.frame) == SearchOptions.SCAN_OK && SearchOptions.getSearchFilter() != null) {
-                    
-                    loadLocalFiles(SearchOptions.getSearchFilter());
-                }
-            }
-        }); 
-        
-        buttonsPanel.add(btnLocal);
-    }
-    
-    private void addCloudDownloadButton(JPanel buttonsPanel) {
-        
-        JButton btnDownload = new JButton();
-        ToolBarUtils.addImageToButton(btnDownload, "Cloud-Download-48.png", "Download", "Download from AWS"); 
-        btnDownload.addMouseListener(new MouseAdapter() {
+	/**
+	 * Default Constructor
+	 * 
+	 * @param application
+	 *            reference to the application.
+	 */
+	public LoadToolBar(CloudTrailViewerApplication application) {
 
-            @Override
-            public void mouseClicked(MouseEvent mouseEvent) {
+		this.application = application;
+		fileChooser.setMultiSelectionEnabled(true);
+		fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
 
-                loadS3files(null);
-            }
-        }); 
-        
-        buttonsPanel.add(btnDownload);
-    }
-    
-    private void addCloudScanButton(JPanel buttonsPanel) {
-        
-        JButton btnDownload = new JButton();
-        ToolBarUtils.addImageToButton(btnDownload, "Cloud-Scan-48.png", "Download", "Scan S3 Files"); 
-        btnDownload.addMouseListener(new MouseAdapter() {
+		buildToolBar();
+	}
 
-            @Override
-            public void mouseClicked(MouseEvent mouseEvent) {
+	////////////////////////////////////////////////////////////////////////////
+	///// private methods
+	////////////////////////////////////////////////////////////////////////////
+	private void buildToolBar() {
 
-                if (SearchOptions.showDialog(CloudTrailViewer.frame) == SearchOptions.SCAN_OK && SearchOptions.getSearchFilter() != null) {
-                    
-                    loadS3files(SearchOptions.getSearchFilter());
-                }
-            }
-        }); 
-        
-        buttonsPanel.add(btnDownload);
-    }
-    
-    private void loadLocalFiles(Filter filter) {
-        
-        int status = fileChooser.showOpenDialog(CloudTrailViewer.frame);
-        if (status == JFileChooser.APPROVE_OPTION) {
+		this.setFloatable(false);
+		this.setLayout(new BorderLayout());
+		this.setBackground(Color.WHITE);
 
-            File[] list;
+		JPanel buttonsPanel = new JPanel();
+		buttonsPanel.setBackground(Color.WHITE);
+		buttonsPanel.setLayout(new BoxLayout(buttonsPanel, BoxLayout.LINE_AXIS));
 
-            if (fileChooser.getSelectedFiles().length != 0)  {
+		addLocalOpenButton(buttonsPanel);
+		addCloudDownloadButton(buttonsPanel);
+		addLocalScanButton(buttonsPanel);
+		addCloudScanButton(buttonsPanel);
 
-                list = fileChooser.getSelectedFiles();
+		this.add(buttonsPanel, BorderLayout.WEST);
+	}
 
-            } else {
+	private void addLocalOpenButton(JPanel buttonsPanel) {
 
-                list = new File[1];
-                list[0] = fileChooser.getSelectedFile();
-            }
+		JButton btnLocal = new JButton();
+		ToolBarUtils.addImageToButton(btnLocal, "Local-Open-48.png", "Load", "Load Local Files");
+		btnLocal.addMouseListener(new MouseAdapter() {
 
-            if (list != null) {
-                 application.loadLocalFiles(list, filter);
-            }
-        }
-    }
-    
-    private void loadS3files(Filter filter) {
-        
-        String query = "SELECT * FROM aws_credentials";
-        List<ResultSetRow> rows = DbManager.getInstance().executeCursorStatement(query);
-        
-        if (!rows.isEmpty()) {
+			@Override
+			public void mouseClicked(MouseEvent mouseEvent) {
 
-            final List<String> files = S3FileChooser.showDialog(CloudTrailViewer.frame);
-            if (!files.isEmpty()) {
-                application.newS3Files(files, filter);
-            }
-            
-        } else {
-            JOptionPane.showMessageDialog(
-                CloudTrailViewer.frame, 
-                "To download file from a S3 bucket you\n will need to define an account in Preferences."
-            );
-        } 
-    }
+				loadLocalFiles(null);
+			}
+		});
+
+		buttonsPanel.add(btnLocal);
+	}
+
+	private void addLocalScanButton(JPanel buttonsPanel) {
+
+		JButton btnLocal = new JButton();
+		ToolBarUtils.addImageToButton(btnLocal, "Local-Scan-48.png", "Local Scan", "Scan Local Files");
+		btnLocal.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent mouseEvent) {
+
+				if (SearchOptions.showDialog(CloudTrailViewer.frame) == SearchOptions.SCAN_OK
+						&& SearchOptions.getSearchFilter() != null) {
+
+					loadLocalFiles(SearchOptions.getSearchFilter());
+				}
+			}
+		});
+
+		buttonsPanel.add(btnLocal);
+	}
+
+	private void addCloudDownloadButton(JPanel buttonsPanel) {
+
+		JButton btnDownload = new JButton();
+		ToolBarUtils.addImageToButton(btnDownload, "Cloud-Download-48.png", "Download", "Download from AWS");
+		btnDownload.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent mouseEvent) {
+
+				loadS3files(null);
+			}
+		});
+
+		buttonsPanel.add(btnDownload);
+	}
+
+	private void addCloudScanButton(JPanel buttonsPanel) {
+
+		JButton btnDownload = new JButton();
+		ToolBarUtils.addImageToButton(btnDownload, "Cloud-Scan-48.png", "Download", "Scan S3 Files");
+		btnDownload.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent mouseEvent) {
+
+				if (SearchOptions.showDialog(CloudTrailViewer.frame) == SearchOptions.SCAN_OK
+						&& SearchOptions.getSearchFilter() != null) {
+
+					loadS3files(SearchOptions.getSearchFilter());
+				}
+			}
+		});
+
+		buttonsPanel.add(btnDownload);
+	}
+
+	// TODO: Change this to check the selected file types. If they are
+	// directories, add all files in the directories to the collection of files
+	// to be opened.
+	private void loadLocalFiles(Filter filter) {
+
+		int status = fileChooser.showOpenDialog(CloudTrailViewer.frame);
+		if (status == JFileChooser.APPROVE_OPTION) {
+
+			File[] list;
+
+			if (fileChooser.getSelectedFiles().length != 0) {
+
+				list = fileChooser.getSelectedFiles();
+
+			} else {
+
+				list = new File[1];
+				list[0] = fileChooser.getSelectedFile();
+			}
+			list = getFileList(list);
+
+			// now check each file for it's type. If it is a directory, open it
+			// and find all the files within...
+
+			if (list != null) {
+				application.loadLocalFiles(list, filter);
+			}
+		}
+	}
+
+	private File[] getFileList(File[] listIn) {
+
+		// If it is null, send it back!
+		if (listIn == null) {
+			return listIn;
+		}
+
+		ArrayList<File> destList = new ArrayList<File>();
+		for (File f : listIn) {
+			digFilesForFiles(f, destList);
+		}
+
+		return destList.toArray(new File[destList.size()]);
+	}
+
+	private void digFilesForFiles(File fileIn, ArrayList<File> destList) {
+
+		if (fileIn.isDirectory()) {
+			for (File subFile : fileIn.listFiles()) {
+				digFilesForFiles(subFile, destList);
+			}
+		} else {
+			destList.add(fileIn);
+		}
+
+	}
+
+	private void loadS3files(Filter filter) {
+
+		String query = "SELECT * FROM aws_credentials";
+		List<ResultSetRow> rows = DbManager.getInstance().executeCursorStatement(query);
+
+		if (!rows.isEmpty()) {
+
+			final List<String> files = S3FileChooser.showDialog(CloudTrailViewer.frame);
+			if (!files.isEmpty()) {
+				application.newS3Files(files, filter);
+			}
+
+		} else {
+			JOptionPane.showMessageDialog(CloudTrailViewer.frame,
+					"To download file from a S3 bucket you\n will need to define an account in Preferences.");
+		}
+	}
 }
